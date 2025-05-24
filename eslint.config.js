8ec8436f5036cc74147f832421b3edb373b4fb20
Laurence Summers <lks@live.co.uk>
2025-05-24 16:54:33 +0100

    Updates dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -6,24 +6,24 @@ import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended"
 import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  pluginJs.configs.recommended,
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  eslintPluginPrettierRecommended,
-  { linterOptions: { reportUnusedInlineConfigs: "error" } },
-  globalIgnores(["./tests/data/testData.ts"]),
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-  {
-    rules: {
-      // suppress errors for missing 'import React' in files
-      "react/react-in-jsx-scope": "off",
-      // allow jsx syntax in js files (for next.js project)
-      "react/jsx-filename-extension": [
-        1,
-        { extensions: [".js", ".jsx", ".ts", ".tsx"] },
-      ],
-      // should add ".ts" if typescript project
+    pluginJs.configs.recommended,
+    tseslint.configs.recommended,
+    pluginReact.configs.flat.recommended,
+    eslintPluginPrettierRecommended,
+    { linterOptions: { reportUnusedInlineConfigs: "error" } },
+    globalIgnores(["./tests/data/testData.ts", "./src/client/src/routes"]),
+    { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+    {
+        rules: {
+            // suppress errors for missing 'import React' in files
+            "react/react-in-jsx-scope": "off",
+            // allow jsx syntax in js files (for next.js project)
+            "react/jsx-filename-extension": [
+                1,
+                { extensions: [".js", ".jsx", ".ts", ".tsx"] },
+            ],
+            // should add ".ts" if typescript project
+        },
     },
-  },
-  { languageOptions: { globals: globals.browser } },
+    { languageOptions: { globals: globals.browser } },
 ]);
--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,33 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import viteTsconfigPaths from "vite-tsconfig-paths";
+import TanStackRouterVite from "@tanstack/router-plugin/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  // depending on your application, base can also be "/"
-  root: "src/client/",
-  base: "",
-  plugins: [react(), viteTsconfigPaths()],
-  build: {
-    outDir: "../../dist/client",
-    emptyOutDir: true,
-  },
-  resolve: {
-    alias: [{ find: "node_modules", replacement: "node_modules" }],
-  },
-  // server: {
-  //   // this ensures that the browser opens upon server start
-  //   open: true,
-  //   proxy: {
-  //     // this allows localhost api calls
-  //     "/api": {
-  //       target: "http://localhost:5000",
-  //       changeOrigin: true,
-  //     },
-  //   },
-  // },
+    // depending on your application, base can also be "/"
+    root: "src/client/",
+    base: "",
+    plugins: [
+        TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
+        react(),
+        viteTsconfigPaths(),
+    ],
+    build: {
+        outDir: "../../dist/client",
+        emptyOutDir: true,
+    },
+    resolve: {
+        alias: [{ find: "node_modules", replacement: "node_modules" }],
+    },
+    // server: {
+    //   // this ensures that the browser opens upon server start
+    //   open: true,
+    //   proxy: {
+    //     // this allows localhost api calls
+    //     "/api": {
+    //       target: "http://localhost:5000",
+    //       changeOrigin: true,
+    //     },
+    //   },
+    // },
 });

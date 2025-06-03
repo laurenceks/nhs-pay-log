cd1945c4f0d360a21fa853c2aa251a0206355428
Laurence Summers <lks@live.co.uk>
2025-06-03 18:53:43 +0100

    Changes what will be db column names to snake case to ensure mysql compatibility

--- types/commonTypes.d.ts
+++ types/commonTypes.d.ts
@@ -18,31 +18,44 @@ export type ShiftExtra = {
     amount: number;
 };
 
-export type LogShift = {
-    actualEnd: string;
+export type ShiftEdit = {
     date: string;
-    double: number;
-    employment: string;
-    extras: { id: ShiftExtra["id"]; quantity: number }[];
-    flat: number;
     from: string;
-    plannedTo: string;
-    actualTo: string;
-    higherRate: number;
+    planned_to: string;
+    actual_to: string;
     id: string;
-    // incentive: number;
-    // incentiveHours: number;
-    // incentiveType: number;
-    lowerRate: number;
-    // mileage: number;
-    // mileageLocation: string;
-    // mileageType: string;
-    overrunType: Overruns;
-    plannedEnd: string;
+    actual_end: string;
+    employment_id: string;
+    extras: ShiftExtra["id"][];
+    overrun_type: Overruns;
+    planned_end: string;
     start: string;
-    timeAndHalf: number;
-    toil: number;
     type: ShiftTypes;
 };
+
+export type USH = {
+    higher_rate: number;
+    lower_rate: number;
+};
+
+export type AdditionalHours = {
+    flat: number;
+    time_and_half: number;
+    toil: number;
+    double: number;
+};
+
+export type CalculatedHours = USH & AdditionalHours;
+
+export type LogShift = AdditionalHours &
+    USH &
+    ShiftEdit & {
+        // incentive: number;
+        // incentiveHours: number;
+        // incentiveType: number;
+        // mileage: number;
+        // mileageLocation: string;
+        // mileageType: string;
+    };
 
 export type ShiftEditModalState = { show: boolean; shift: LogShift | null };

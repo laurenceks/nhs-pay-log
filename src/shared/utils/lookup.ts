ade500b95f25772850a2ed91d6dd16a09a9013f0
Laurence Summers <lks@live.co.uk>
2025-06-03 18:59:29 +0100

    Adds employment_id criteria, filterOptionsByDate

--- src/shared/utils/lookup.ts
+++ src/shared/utils/lookup.ts
@@ -1,16 +1,28 @@
 import { ShiftExtra } from "../../../types/commonTypes";
+import { formatDate } from "./formatDates.ts";
 
-export const lookupByDate = <T>(
-    arr: ({ from?: string | Date | number; to?: string | Date | number } & T)[],
-    d: string | Date | number,
-    returnKey?: keyof T
-) => {
+export const lookupByDate = <T>({
+    arr = [],
+    d = new Date(),
+    returnKey,
+    employment_id,
+}: {
+    arr: ({
+        from?: string | Date | number;
+        to?: string | Date | number;
+        employment_id: string;
+    } & T)[];
+    d: string | Date;
+    employment_id: string;
+    returnKey?: keyof T;
+}) => {
+    const dSorting = formatDate(d, "yyyy-mm-dd");
     const result = arr.find((x) => {
-        //TODO validate string dates
         return (
-            (!x.from && x.to && x.to >= d) ||
-            (!x.to && x.from && x.from <= d) ||
-            (x.to && x.from && x.to >= d && x.from <= d)
+            ((!x.from && x.to && x.to >= dSorting) ||
+                (!x.to && x.from && x.from <= dSorting) ||
+                (x.to && x.from && x.to >= dSorting && x.from <= dSorting)) &&
+            (!employment_id || x.employment_id === employment_id)
         );
     });
     if (result && returnKey && returnKey in result) {
@@ -34,5 +46,24 @@ export const lookupShiftExtra = (
                     (!x.from && !x.to))
             );
         })?.amount || 0
+    );
+};
+
+export const filterOptionsByDate = <T>(
+    arr: ({
+        from?: string | Date | number;
+        to?: string | Date | number;
+        employment_id?: string;
+    } & T)[] = [],
+    d: string | Date = new Date(),
+    employment_id?: string
+) => {
+    const dSorting = formatDate(d, "yyyy-mm-dd");
+    return arr.filter(
+        (x) =>
+            ((!x.from && x.to && x.to >= dSorting) ||
+                (!x.to && x.from && x.from <= dSorting) ||
+                (x.to && x.from && x.to >= dSorting && x.from <= dSorting)) &&
+            (!employment_id || x.employment_id === employment_id)
     );
 };
